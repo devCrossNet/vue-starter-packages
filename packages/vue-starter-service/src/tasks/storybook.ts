3fec2cac5b7843cee8d86c17291a2f6cbc29c1b9
Johannes Werner <johannes.werner86@gmail.com>
2019-01-09 22:41:03 +0100

    fix(storybook): change absolute to relative paths

--- packages/vue-starter-service/src/tasks/storybook.ts
+++ packages/vue-starter-service/src/tasks/storybook.ts
@@ -1,6 +1,5 @@
 import { runProcess } from '../utils/process';
 import { logErrorBold } from '../utils/ui';
-import { packageRoot, runtimeRoot } from '../utils/path';
 
 export const StorybookTask = async (command: any) => {
   let args = command.parent.rawArgs.splice(3);
@@ -13,10 +12,10 @@ export const StorybookTask = async (command: any) => {
     args.pop();
     args = args.concat(['-p', '6006']);
   } else {
-    args = args.concat(['--output-dir', runtimeRoot('storybook-static')]);
+    args = args.concat(['--output-dir', './storybook-static']);
   }
 
-  args = args.concat(['--config-dir', runtimeRoot('.vue-starter/storybook')]);
+  args = args.concat(['--config-dir', './.vue-starter/storybook']);
 
   try {
     await runProcess(binary, args);
